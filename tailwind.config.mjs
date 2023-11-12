29c1709d2864d4f8cd93d7b83762f01ea4a9646a
Eamon Taaffe <7099155+eamontaaffe@users.noreply.github.com>
2023-11-12 12:46:37 +1100

    Started laying out the website

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+        fontFamily: {
+          'display': ['ABCBingo', 'sans-serif'],
+          'body': ['Maria', 'Helvetica', 'sans-serif'],
+        },
+        colors: {
+          background: '#d8d1c6',
+        },
+	},
+	plugins: [],
+}
